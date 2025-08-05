fefd39c2fa78f19aca86af32a48db44675d1f932
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-08-06 00:04:32 +0900

    fix: client

--- client/src/handler/auth.ts
+++ client/src/handler/auth.ts
@@ -1,4 +1,22 @@
 const loginBtn = document.querySelector<HTMLButtonElement>("#login")!;
+const accessTokenInput =
+  document.querySelector<HTMLInputElement>("#access_token")!;
+const accessTokenExpInput =
+  document.querySelector<HTMLInputElement>("#access_token_exp")!;
+const refreshTokenInput =
+  document.querySelector<HTMLInputElement>("#refresh_token")!;
+const refreshTokenExpInput =
+  document.querySelector<HTMLInputElement>("#refresh_token_exp")!;
+
+type Token = {
+  value: string;
+  exp: string;
+};
+
+type ResponseData = {
+  access_token: Token;
+  refresh_token: Token;
+};
 
 export function authHandler() {
   loginBtn.addEventListener("click", actionLogin);
@@ -9,13 +27,20 @@ async function actionLogin() {
   const username = "dummy";
 
   try {
-    await fetch("http://localhost:8000/auth/login", {
+    const response = await fetch("http://localhost:8000/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ username, password }),
     });
+
+    const responseData: ResponseData = await response.json();
+
+    accessTokenInput.value = responseData.access_token.value;
+    accessTokenExpInput.value = responseData.access_token.exp;
+    refreshTokenInput.value = responseData.refresh_token.value;
+    refreshTokenExpInput.value = responseData.refresh_token.exp;
   } catch (err) {
     console.error(err);
   }
--- client/src/handler/payment.ts
+++ client/src/handler/payment.ts
@@ -1,5 +1,7 @@
 const paymentBtn = document.querySelector<HTMLButtonElement>("#payment")!;
 const priceEl = document.querySelector<HTMLInputElement>("#price")!;
+const accessTokenInput =
+  document.querySelector<HTMLInputElement>("#access_token")!;
 
 export function paymentHandler() {
   paymentBtn.addEventListener("click", actionPayment);
@@ -8,13 +10,23 @@ export function paymentHandler() {
 async function actionPayment() {
   const price = Number(priceEl.value);
   if (price <= 0) {
-    console.error("1円以上で選択してください");
+    alert("1円以上で選択してください");
+    return;
+  }
+
+  const accessToken = accessTokenInput.value;
+  if (!accessToken) {
+    alert("ログインしてください");
     return;
   }
 
   try {
     await fetch("http://localhost:8000/payment/send", {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
       body: JSON.stringify({ price }),
     });
   } catch (err) {
